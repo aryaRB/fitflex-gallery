bdb99ec99c99b795417312c0d75f87bb15f8f521
ARYA <aryaRbarua@gmail.com>
2020-05-13 11:30:13 +0530

    added corousal

---
+++ bootdemo.js
@@ -0,0 +1,23 @@
+
+    // slider
+    
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 180,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows : true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+      });
