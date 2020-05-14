b39f2b5d52c697b16ded3fb73212bf1abf18aab3
ARYA <aryaRbarua@gmail.com>
2020-05-14 20:51:00 +0530

    renew

--- bootdemo.js
+++ bootdemo.js
@@ -1,24 +1,36 @@
 
-    slider 3d
+    // slider 3d
     
-    var swiper = new Swiper('.swiper-container', {
+    var swiper = new Swiper('.swiper1', {
         effect: 'coverflow',
         grabCursor: true,
         centeredSlides: true,
         slidesPerView: 'auto',
         coverflowEffect: {
-          rotate: 50,
+          rotate: 60,
           stretch: 0,
-          depth: 100,
+          depth: 80,
           modifier: 1,
           slideShadows : true,
         },
         pagination: {
-          el: '.swiper-pagination',
+          el: '.swiper-pagination1',
         },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
           },
       });
+      
+//mouse vertical slider
+      var swiper = new Swiper('.swiper2', {
+        direction: 'vertical',
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
+        pagination: {
+          el: 'swiper-pagination1',
+          clickable: true,
+        },
+    });
 
